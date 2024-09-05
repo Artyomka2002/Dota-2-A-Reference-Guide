72278586384f5d600e1a5fb2531e4a28b40fbf80
Artyomka2002 <105603537+Artyomka2002@users.noreply.github.com>
2024-09-05 12:45:05 +0700

    Сообщение о коммите

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// vite.config.js
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Убедитесь, что выходная директория правильная
+    rollupOptions: {
+      output: {
+        // Проверьте, чтобы настройки здесь были корректные
+      },
+    },
+  },
+});
