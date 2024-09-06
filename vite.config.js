064d309051bc503d05735bf21fb1b474b6c6b5b4
Artyomka2002 <ilyichev.a.a@mail.ru>
2024-09-06 21:13:07 +0700

    add new dist

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dota-2-A-Reference-Guide/', // Проверьте правильность пути
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js', // Убедитесь, что все файлы имеют расширение .js
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
-  },
+  // base: '/Dota-2-A-Reference-Guide/', // Проверьте правильность пути
+
 });
 
