4f4322c90da5d433f059d6a201ed3bdb95d28a1c
Artyomka2002 <105603537+Artyomka2002@users.noreply.github.com>
2024-09-06 19:24:09 +0700

    Добавление loading

--- src/pages/InformationAboutTheHeroes/InformationAboutTheHeroes.jsx
+++ src/pages/InformationAboutTheHeroes/InformationAboutTheHeroes.jsx
@@ -1,50 +1,53 @@
-import { useState, useMemo } from 'react';
-import cl from './InfotmationAboutTheHeroes.module.css'
-import { useSortImagesHeroYouTube } from '../../hooks/useSortImagesYoutube';
+import { useState, useMemo } from "react";
+import cl from "./InfotmationAboutTheHeroes.module.css";
+import { useSortImagesHeroYouTube } from "../../hooks/useSortImagesYoutube";
 // import ErrorScreen from '../../UI/ErrorScreen/ErrorScreen';
-import { useParams } from 'react-router-dom';
-import Modal from '../../UI/MyModal/modal';
-import useFetch from '../../hooks/useFeatch';
+import { useParams } from "react-router-dom";
+import Modal from "../../UI/MyModal/modal";
+import useFetch from "../../hooks/useFeatch";
 // import BoxRightContent from './informationAboutTheHeroesComponents/BoxRightContent';
-import BoxRightContent from './informationAboutTheHeroesComponents/BoxRightContent';
-import CollapseContainer from './informationAboutTheHeroesComponents/collapseContainer';
-import NestedContent from './informationAboutTheHeroesComponents/nestedContend';
-import { links } from '../../JsonStorage/objectSort';
-import { sort } from '../../common';
+import BoxRightContent from "./informationAboutTheHeroesComponents/BoxRightContent";
+import CollapseContainer from "./informationAboutTheHeroesComponents/collapseContainer";
+import NestedContent from "./informationAboutTheHeroesComponents/nestedContend";
+import { links } from "../../JsonStorage/objectSort";
+import { sort } from "../../common";
+import LoadingSpinner from "../../UI/Loading/Loading";
 // import LoadingScreen from '../../UI/loading/loading';
 
 // название о 1 герое , s
 const InformationAboutTheHeroes = () => {
-    const { id } = useParams()
-    const { dateded } = useSortImagesHeroYouTube()
-    const [modal, setModal] = useState(false)
-    const { data, loading, error } = useFetch(`https://api.opendota.com/api/heroStats/`)
+  const { id } = useParams();
+  const { dateded } = useSortImagesHeroYouTube();
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useFetch(
+    `https://api.opendota.com/api/heroStats/`
+  );
 
-    const dataSorce = useMemo(() => {
-        if (!data && id) return null
-        return data.find((i) => i.id == id);
-    }, [id, data])
+  const dataSorce = useMemo(() => {
+    if (!data && id) return null;
+    return data.find((i) => i.id == id);
+  }, [id, data]);
 
-    // if (loading) return <LoadingScreen />
-    if (!dataSorce) return null
-    // if (error) return <ErrorScreen/>
+  if (!dataSorce) return <LoadingSpinner />;
 
-    return (
-        //    <>
-        //    <NestedContent dataSorce={dataSorce}/>
-        //     {dataSorce.localized_name } , Айди элемента 
-        //    </>
-        <main className={cl.main}>
-            <div style={{ display: 'flex', flexDirection: 'column', padding:'10px' }}>
-                <NestedContent dataSorce={dataSorce} sort={sort(dataSorce, links)} />
-                <CollapseContainer dataSorce={dataSorce} dateded={dateded} id={id} />
-            </div>
-            <BoxRightContent dataSorce={dataSorce} id={id} />
-        </main >
-    )
-}
-export default InformationAboutTheHeroes
-
+  
+  return (
+    //    <>
+    //    <NestedContent dataSorce={dataSorce}/>
+    //     {dataSorce.localized_name } , Айди элемента
+    //    </>
+    <main className={cl.main}>
+      <div
+        style={{ display: "flex", flexDirection: "column", padding: "10px" }}
+      >
+        <NestedContent dataSorce={dataSorce} sort={sort(dataSorce, links)} />
+        <CollapseContainer dataSorce={dataSorce} dateded={dateded} id={id} />
+      </div>
+      <BoxRightContent dataSorce={dataSorce} id={id} />
+    </main>
+  );
+};
+export default InformationAboutTheHeroes;
 
 //    className={cl.containerLim}
 //     <Modal setVisible={setModal} visible={modal} cross={'×'}>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dota-2-A-Reference-Guide/', // Проверьте правильность пути
+  // base: '/Dota-2-A-Reference-Guide/', // Проверьте правильность пути
   build: {
     outDir: 'dist',
     rollupOptions: {
